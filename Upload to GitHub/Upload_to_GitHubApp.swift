7008ea33a69bb87339f7227389ee9b0434acf783
Andrii <ckifix@icloud.com>
2023-02-18 21:19:08 +0200

    Initial Commit

---
+++ Upload to GitHub/Upload_to_GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Upload_to_GitHubApp.swift
+//  Upload to GitHub
+//
+//  Created by Andrii Zlenko on 18.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct Upload_to_GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
